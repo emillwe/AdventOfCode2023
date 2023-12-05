a6d2e5db6aaceb2c4e19de2da45a18c905a53ea2
Emmett Miller <emmett.miller@gmail.com>
2023-12-04 22:10:28 -0800

    Day2 soln. cont'd

--- Day2/pt1/day2.cpp
+++ Day2/pt1/day2.cpp
@@ -10,14 +10,19 @@ int MAX_RED = 12;
 int MAX_GREEN = 13;
 int MAX_BLUE = 14;
 
-int stringToIndex(string& indexStr) {
-	char ASCII_OFFSET = '0';
+char ASCII_OFFSET = '0';
+
+int charToDigit(char c) {
+	return c - ASCII_OFFSET;
+}
+
+int stringToInt(string& indexStr) {
 	int numDigits = indexStr.length();
 	int result = 0;
 	int place = 1;
 	for (auto itr = indexStr.rbegin(); itr < indexStr.rend(); ++itr) {
 		char digitChar = *itr;
-		int digit = digitChar - ASCII_OFFSET;
+		int digit = charToDigit(digitChar);
 		result += (digit * place);
 		place *= 10;
 	}
@@ -25,24 +30,78 @@ int stringToIndex(string& indexStr) {
 	return result;
 }
 
-// process whether this number of cubes is valid
-bool processCubes(string& nextOfLine) {
+void trimLeadingWhitespace(string& str) {
+	if (isspace(str[0])) {
+		str = str.substr(1, str.length());
+	}
+}
+
+// for this round,
+// update max dice for each color
+void processRound(string& line, int *rPtr, int *gPtr, int *bPtr, int *maxPtr) {
+	// split draw into colors: deliminate by ','
+	istringstream iss2(line);
+	string thisDraw;
+	
+	while(getline(iss2, thisDraw, ',')) {
+		trimLeadingWhitespace(thisDraw);
+				
+		istringstream iss3(thisDraw);
+	
+		string color;
+		string digits;
+		
+		iss3 >> digits;
+		int numCubes = stringToInt(digits);
+		
+		iss3 >> color;
+				
+		// check color
+		if (color == "red") {
+			maxPtr = rPtr;
+		}
+		else if (color == "green") {
+			maxPtr = gPtr;
+		} else { // color == "blue"
+			maxPtr = bPtr;
+		}
+		
+		*maxPtr = max(numCubes, *maxPtr);
+		
+		cout << *rPtr << " " << *gPtr << " " << *bPtr << endl;
+	}
+}
+
+// process whether this game is valid
+bool processGame(string& line) {
 	// running max of RGB cubes
+	int redMax, greenMax, blueMax = 0;
+	int *maxPtr;
 	
 	// split by draw: deliminate by ';'
-	// -> string draw
+	istringstream iss(line);
+	string drawCubes;
 	
-	// split draw into colors: deliminate by ','
-	// -> string colors
+	while (getline(iss, drawCubes, ';')) {
+		// remove leading whitespace
+		trimLeadingWhitespace(drawCubes);
+		
+		// process this round of the game
+		processRound(drawCubes, &redMax, &greenMax, &blueMax, maxPtr);
+	}
 	
-	// pointer to int for max
-	// switch color to decide which max to point to
-	// check max
-	
-	// end of line: for each color, if max > spec max, return false
-	// (outside loop) return true
-	
-	return false;
+	// end of game. Is it valid?
+	if (redMax > MAX_RED) {
+		return false;
+	}
+	else if (greenMax > MAX_GREEN) {
+		return false;
+	}
+	else if (blueMax > MAX_BLUE) {
+		return false;
+	} else { // all valid
+		return true;
+	}
 }
 
 // read thorugh input, process lines, get solution
@@ -61,17 +120,22 @@ void readLines(string fileName) {
 		if (!nextLine.empty()) {
 			istringstream iss(nextLine);	// read words from line
 			string nextWord;
-			getline(iss, nextWord, ' ');	// read "Game"
-			getline(iss, nextWord, ' ');	// read "<Game index>:"
-			nextWord.pop_back();			// "<Game Index>"
+			
+			for (int i = 0; i < 2; ++i) {
+				iss >> nextWord;
+			}
+			
+			nextWord.pop_back();	// trim ':'
 			
 			// get game index
-			int gameIndex = stringToIndex(nextWord);
+			int gameIndex = stringToInt(nextWord);
+			
+			cout << "Game " << gameIndex << ": " << endl;
 			
 			string restOfLine;
 			getline(iss, restOfLine);
 
-			cout << processCubes(nextOfLine) << endl;
+			cout << processGame(restOfLine) << endl;
 			
 		}
 	}
